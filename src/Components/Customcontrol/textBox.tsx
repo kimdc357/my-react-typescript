544aa4aed5bb61ba149b40eb64eaf0b0cd43ef1d
my-react-typescript <dacheng357@163.com>
2020-12-07 14:41:05 +0800

    新建项目

---
+++ src/Components/Customcontrol/textBox.tsx
@@ -0,0 +1,5 @@
+import React,{ Component } from 'react'
+
+class TextBox extends Component{
+    
+}
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Homepage from './Page/Homepage'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Homepage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
